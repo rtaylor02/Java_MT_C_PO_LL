02ddcc1317f589ae0ef08c6ccf79de2ee97412e6
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-01-25 22:18:08 +0000

    Add more info on using AtomicInteger instead in Data Sharing section

--- mt_c/code/3 - Data Sharing between Threads/src/Main3.java
+++ mt_c/code/3 - Data Sharing between Threads/src/Main3.java
@@ -1,7 +1,11 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
 /*
 This code demonstrate racing condition where the incrementing thread and decrementing thread are both accessing
 non-atomic operation which leads to inconsistent state. With atomic operation (using synchronized section),
-result will be consistent 0.
+result will be consistent 0. To make the operation atomic:
+1. use 'synchronized' keyword on method increment() and decrement, or
+2. use AtomicInteger instead of int for inventory.data
  */
 public class Main3 {
     public static void main(String[] args) throws InterruptedException {
@@ -32,19 +36,23 @@ public class Main3 {
         decrementingThread.join();
 
         System.out.println(String.format("inventory.data = %d", inventory.data));
+        //System.out.println(String.format("inventory.data = %d", inventory.data.intValue()));
     }
 
     private static class Inventory {
+        //private AtomicInteger data = new AtomicInteger(0);
         private int data = 0;
 
         public void increment() {
         //public synchronized void increment() {
             data++;
+            //data.incrementAndGet();
         }
 
         public void decrement() {
         //public synchronized void decrement() {
             data--;
+            //data.decrementAndGet();
         }
     }
 }
